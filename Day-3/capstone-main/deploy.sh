ed64eea513d0af6e650e446a7f9499ba4cbf1d41
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-03-22 13:39:10 +0530

    user and password added

--- Day-3/capstone-main/deploy.sh
+++ Day-3/capstone-main/deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u  -p 
+    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
     docker tag test ar8888/dev
     docker push ar8888/dev
     docker-compose up -d
