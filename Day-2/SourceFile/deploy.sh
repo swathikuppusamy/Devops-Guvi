3e728d9a658a0860cd1a2623780a75923a0f150f
swathikuppusamy <swathikuppusamy2005@gmail.com>
2025-03-22 12:03:14 +0530

    Sourcefile commiy

---
+++ Day-2/SourceFile/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+    echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
+    docker tag test ar8888/dev
+    docker push ar8888/dev
+    docker-compose up -d
+    
